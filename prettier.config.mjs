e3110feedd72295af01a2c446e9a47c6235276bd
Gavin Smith <me@gavinhsmith.com>
2025-04-11 14:52:49 -0500

    Fix styling + add author

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,7 @@
 export default {
-    printWidth: 120,
-    tabWidth: 4,
-    singleQuote: false,
-    bracketSameLine: false,
-    trailingComma: "es5",
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSameLine: false,
+  trailingComma: "es5",
 };
